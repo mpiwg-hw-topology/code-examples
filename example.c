0d3048045a837cdddacbbb68833950d25a2e7a73
Dan Holmes <daniel.john.holmes@intel.com>
2023-07-06 18:03:37 +0100

    Update example.c

--- example.c
+++ example.c
@@ -13,9 +13,38 @@ int main(int rgc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &mysize);
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   
+  MPI_Comm my_cart_comm; // normal MPI
+
+  #if USE_WAIT
+  MPI_Request comm_create_request; // normal MPI
+  int reorder = 1;
+  MPI_Icart_create(..., reorder, ..., &my_cart_comm, &comm_create_request); //new
+  #else
+  MPI_Icart_create(..., reorder, ..., &my_cart_comm); //new
+  #endif
+
+  MPI_Neighor_alltoallw_init(..., my_cart_comm, &halo_exchange_request); // normal MPI
+  MPI_Allreduce_init(..., my_cart_comm, &reduction_request); // normal MPI
+  
+  #if USE_WAIT
+  MPI_Wait(&comm_create_request, MPI_STATUS_IGNORE); //new
+  #else
+  MPI_Comm_commit(&my_cart_comm, MPI_INFO_NULL); //new
+  #endif
+  
+  // normal MPI
+  MPI_Start(&halo_exchange_request);
+  MPI_Start(&reduction_request);
+  MPI_Wait(&halo_exchange_request);
+  MPI_Wait(&reduction_request);
+  MPI_Request_free(&halo_exchange_request);
+  MPI_Request_free(&reduction_request);
+
+  MPI_Comm_disconnect(&my_cart_comm);
+
+  
   MPI_Topology virtual_topo;
   MPI_Topology hw_topo;
-
 
   /*1- build virtual topo */
   MPI_Topo_create(MPI_CART, mysize, myrank,  &virtual_topo);
